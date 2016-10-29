a7d68df4043fd140cd4e8739d7aac1a29e9cccb2
SamTebbs33 <samuel.tebbs@gmail.com>
2016-10-29 18:38:38 +0100

    Added script execution on button press

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var request = require("request");
 var dash_button = require("node-dash-button");
+var fork = require('child_process').fork;
 
 NERF_BUTTON_MAC = "50:f5:da:97:6f:59";
 var dash = dash_button([NERF_BUTTON_MAC], null, null, 'all');
+var script = null;
 
-console.log("Listening...");
+function log(msg) {
+	console.log("> " + msg);
+}
+
+log("Listening for presses");
 dash.on("detected", function(dash_id) {
-    console.log("Packet from " + dash_id);
+	log("Press from " + dash_id);
+	if(script) {
+		log("Launching " + script);
+		var proc = fork("./" + script);
+	} else log("No script set");
+});
+
+var stdin = process.openStdin();
+stdin.addListener("data", function(d) {
+	var input = d.toString().trim();
+	log("Setting script to " + input);
+	script = input;
 });
